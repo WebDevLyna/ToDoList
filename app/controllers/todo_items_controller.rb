1ce6f8fb1b03954592569878abe07c261cf9f4ca
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-03-12 14:36:08 -0500

    Add action to set_todo_list and todo_item will be redirected to @todo_list

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,8 +1,10 @@
 class TodoItemsController < ApplicationController
-  
+  before_action :set_todo_list
+
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.create(params[:todo_item_params]
+    @todo_item = @todo_list.todo_items.create(:todo_item_params)
+
+    redirect_to @todo_list
   end
 
   private
